793cc485ad02a33ae44586a18d68934fb7e535a7
Rich Searle <richard.searle@spongeuk.com>
2019-05-01 17:29:29 +0100

    some typings and structure

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,76 @@
 
+export interface AudioTimeRange {
+   start:number;
+   end:number;
+}
+
+export enum EaseType {
+   NONE = 0,
+   LINEAR,
+   EXPONENTIAL
+}
+
+export interface InterpolationValueAndEase {
+   value:number;
+   ease?:EaseType;
+}
+
 export interface OscillatorConfig {
+   frequency:number|AudioTimeRange;
+   attack?:InterpolationValueAndEase;
+   decay?:InterpolationValueAndEase;
+}
+
+
+//https://developer.mozilla.org/en-US/docs/Web/API/BiquadFilterNode
+export interface BiquadFilterConfig {
 
 }
 
 export interface SoundConfig {
-   oscillator:OscillatorConfig|OscillatorConfig[];
-      
+   oscillator: OscillatorConfig|OscillatorConfig[];
+   biquadFilters?:BiquadFilterConfig|BiquadFilterConfig[];
 }
-
 
 export class Sound {
 
-   constructor(audioContext :AudioContext, config:SoundConfig ){
+   oscillators:OscillatorNode[] = [];
+   biquadFilters:BiquadFilterNode[] = [];
+   outputNode:AudioNode;
+   context: AudioContext;
 
+   constructor(audioContext: AudioContext, outputNode:AudioNode, config: SoundConfig ) {
+      this.context = audioContext;
+      this.outputNode = outputNode;
+
+      if(config.oscillator){
+         //we have oscilator config(s)
+         if(config.oscillator instanceof Array){
+            //multiple oscilators
+         
+         
+         
+         } else {
+            //single oscilator.
+
+
+         }
+
+      }
+      if(config.biquadFilters){
+         //we have biquadFilter config(s)
+         if(config.biquadFilters instanceof Array){
+            //multiple biquadFilter
+         
+         
+         
+         } else {
+            //single biquadFilters.
+
+
+         }
+
+      }
    }
 
 }
@@ -22,20 +80,22 @@ export default class SoundTests {
 
       console.clear();
 
-      document.body.innerHTML = 
+      document.body.innerHTML =
       `
       <div class="ui">
       <button id="playButton">Start</button>
       </div>
       `;
 
-
-      const button: HTMLButtonElement = document.getElementById("playButton") as HTMLButtonElement;
+      const button: HTMLButtonElement = document.getElementById('playButton') as HTMLButtonElement;
+      
+      //@ts-ignore
       const audioContext: AudioContext = new window.AudioContext();
+
 
       const master_out: GainNode = audioContext.createGain();
       master_out.connect(audioContext.destination);
-      master_out.gain.setValueAtTime(1, audioContext.currentTime)
+      master_out.gain.setValueAtTime(1, audioContext.currentTime);
 
       const compressor: DynamicsCompressorNode = audioContext.createDynamicsCompressor();
       compressor.threshold.value = 0.5;
@@ -47,27 +107,24 @@ export default class SoundTests {
 
       button.onmouseup = () => {
 
-
-         let oscillator: OscillatorNode = audioContext.createOscillator();
-         let osc_volume: GainNode = audioContext.createGain();
+         const oscillator: OscillatorNode = audioContext.createOscillator();
+         const osc_volume: GainNode = audioContext.createGain();
          oscillator.type = 'sine';
          oscillator.frequency.setValueAtTime(70, audioContext.currentTime); // value in hertz
 
-         //fade out on end of sound.
+         // fade out on end of sound.
          osc_volume.gain.setValueAtTime(1, audioContext.currentTime);
          osc_volume.gain.exponentialRampToValueAtTime(0.0001, audioContext.currentTime + 0.4);
-
 
          oscillator.connect(osc_volume);
          osc_volume.connect(compressor);
 
          oscillator.start();
-         oscillator.onended
+         oscillator.onended;
          oscillator.stop(audioContext.currentTime + 0.4);
-      }
+      };
    }
 
-
-};
+}
 
 new SoundTests();
